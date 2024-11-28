fbbe7fba9e1c4ed806653335aa4939c644fa0bbd
romano <romanong321@gmail.com>
2024-11-28 17:30:34 -0400

    deploy funciones en equipo correccion marcas service

--- src/articulo-general-references/marca/marca.service.ts
+++ src/articulo-general-references/marca/marca.service.ts
@@ -140,7 +140,7 @@ export class MarcaService {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     try {
-      await queryRunner.query(`TRUNCATE TABLE marcas RESTART IDENTITY CASCADE`);
+      await queryRunner.query(`TRUNCATE TABLE "marcas" RESTART IDENTITY CASCADE`);
     } catch (error) {
       this.handleDBExceptions(error);
     } finally {
