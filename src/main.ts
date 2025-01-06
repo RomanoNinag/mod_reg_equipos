d708b3381eb050243792c98a50520db12f01a57b
romano <romanong321@gmail.com>
2025-01-06 14:21:46 -0400

    veriging cd

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
   // await app.listen(3000);
   // await app.listen(parseInt(process.env.PORT) || 3001);
   await app.listen();
-  logger.log('MS Articulo is running');
+  logger.log('MS Arma Equipo is running');
 }
 bootstrap();
